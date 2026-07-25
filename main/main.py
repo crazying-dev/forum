35e29e479a339de971b916bd6ff9a3b7a0d39e05
crazying-dev <3890320020@qq.com>
2026-07-25 21:31:07 +0800

    修复API的500错误

--- main/main.py
+++ main/main.py
@@ -1137,7 +1137,7 @@ def navifox_avatar():return ""
 # ─────── 众生之门API ───────
 @app.route("/TheDoorOfBings/UUID4/")
 def TheDoorOfBings_UUID():
-	return uuid.uuid4()
+	return str(uuid.uuid4())
 
 
 if __name__ == '__main__':
