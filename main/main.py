f9d47a69591795acf23d53e2ee05c89b3c37ab47
crazying-dev <3890320020@qq.com>
2026-07-26 19:25:14 +0800

    修复路由

--- main/main.py
+++ main/main.py
@@ -1127,6 +1127,7 @@ def QQ_redirect():
 	return redirect("https://qm.qq.com/q/bLxr68HnUI")
 
 @app.route("/INFO/")
+@app.route("/INFO")
 def INFO():
 	return """妖精论坛——一个充满神秘色彩的封闭区域，在此处，你会与聚灵而生的妖精，亦或者得到某种机遇而打开修行之路的人类，展开全新的相遇"""
 
