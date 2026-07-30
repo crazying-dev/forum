5649d6ad921819c269f32993e96502f21aa6bbac
crazying-dev <3890320020@qq.com>
2026-07-30 20:17:44 +0800

    我是神人

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ app = flask.Flask(__name__)
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
-    return "网站已迁移,新网址为<a href=\"https://www.yjlt.top\"></a>"
+    return "网站已迁移,新网址为<a href=\"https://www.yjlt.top\">https://www.yjlt.top</a>"
 
 if __name__ == '__main__':
     app.run()
