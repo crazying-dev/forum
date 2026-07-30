2875b1f736dd2612e59133b902d35fce94277342
crazying-dev <3890320020@qq.com>
2026-07-30 19:46:42 +0800

    重定向

--- index.py
+++ index.py
@@ -1,6 +1,10 @@
-import sys
-sys.path.append("./main")
-from main.main import app
+import flask
+app = flask.Flask(__name__)
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return "网站已迁移,新网址为<a href=\"https://www.yjlt.top\"></a>"
 
 if __name__ == '__main__':
     app.run()
