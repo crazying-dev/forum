06f31ad25bf5b11afbcd305e7edf8470fce7f76d
crazying-dev <3890320020@qq.com>
2026-08-27 08:24:12 +0800

    Updata New with version v2.5.5 Font Awesome

--- static/sw.js
+++ static/sw.js
@@ -1,9 +1,8 @@
 // 妖精论坛 Service Worker - 离线缓存静态资源
-var CACHE_NAME = 'forum-new-v4';
+var CACHE_NAME = 'forum-new-v5';
 var CACHE_URLS = [
   '/',
   '/forum',
-  '/static/css/font-awesome.min.css',
   '/static/css/main.css?v=9',
   '/static/js/marked.min.js',
   '/static/js/AfterBody.js?v=9',
