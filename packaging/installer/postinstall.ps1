578b5f9ea14d40f7bfa75a0002b2349c9b53a7e9
crazying-dev <3890320020@qq.com>
2026-09-26 21:31:55 +0800

    版本号升至 1.3.1
    - app/__init__.py、packaging/version.json、packaging/version_info.txt
    - 安装包 Inno Setup 脚本 CrForum.iss 与 postinstall.ps1
    - 安装包文件属性版本号 1.3.1.0 / 产品版本 1.3.1

--- packaging/installer/postinstall.ps1
+++ packaging/installer/postinstall.ps1
@@ -4,7 +4,7 @@
 param(
     [Parameter(Mandatory = $true)][string]$Exe,
     [Parameter(Mandatory = $true)][string]$AppDir,
-    [string]$Version = '1.3.0',
+    [string]$Version = '1.3.1',
     [switch]$NoStartMenu,
     [switch]$DesktopShortcut
 )
